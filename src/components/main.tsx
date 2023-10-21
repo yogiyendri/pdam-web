55ea1c7865146360149ae66e628b1d3988fc3017
yogiyendri <yogiyendrisaputra@gmail.com>
2023-10-21 18:23:12 +0700

    first commit

---
+++ src/components/main.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Slide from "./slide";
+
+export default function Main() {
+  return (
+    <main className="flex-1">
+      <div className="relative">
+        {/* Banner Slide */}
+        <Slide />
+      </div>
+    </main>
+  );
+}
---
+++ src/components/slide.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+export default function Slide() {
+  return (
+    <section>
+      <div className="animate-show">
+        <div className="relative">Slide</div>
+      </div>
+    </section>
+  );
+}
